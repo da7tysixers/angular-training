95a9be43c3aa14c7ca6f725f4b41592f4fe52811
Emmanuel Orubele <blontf@gmail.com>
2020-09-29 00:26:15 -0400

    angular typescript sales project

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -3,15 +3,15 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
 
   // create an array of salesperson object
   salesPersonList: SalesPerson[] = [
-     new SalesPerson("Anup", "Kumar", "anup.kumar@gmail.com", 5000),
-     new SalesPerson("Johnson", "smith", "johnson.smith@gmail.com", 4000),
+     new SalesPerson("Anup", "Kumar", "anup.kumar@gmail.com", 50000),
+     new SalesPerson("Johnson", "smith", "johnson.smith@gmail.com", 40000),
      new SalesPerson("John", "Doe", "john.doe@gmail.com", 90000),
      new SalesPerson("Claire", "Murphy", "claire.murphy@gmail.com", 60000)
   ];
